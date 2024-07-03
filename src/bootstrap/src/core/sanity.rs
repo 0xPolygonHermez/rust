d23923993d6f3c78ad3c3d80f8bb0704ad83ef83
Eduardo Antuña Díez <eduadiez@gmail.com>
2024-07-03 17:24:33 +0000

    First commit

--- src/bootstrap/src/core/sanity.rs
+++ src/bootstrap/src/core/sanity.rs
@@ -39,6 +39,8 @@ pub struct Finder {
 // Targets can be removed from this list once they are present in the stage0 compiler (usually by updating the beta compiler of the bootstrap).
 #[cfg(not(feature = "bootstrap-self-test"))]
 const STAGE0_MISSING_TARGETS: &[&str] = &[
+    "riscv64ima-polygon-ziskos-elf",
+    "riscv32ima-polygon-ziskos-elf"
     // just a dummy comment so the list doesn't get onelined
 ];
 
