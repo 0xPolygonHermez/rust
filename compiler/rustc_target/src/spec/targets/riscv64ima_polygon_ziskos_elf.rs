39c9b50268776c241d743032c22c555cdde7e0ea
Eduardo Antuña Díez <eduadiez@gmail.com>
2024-07-16 18:47:24 +0000

    add linker script and add ubuntu as self-hosted

--- compiler/rustc_target/src/spec/targets/riscv64ima_polygon_ziskos_elf.rs
+++ compiler/rustc_target/src/spec/targets/riscv64ima_polygon_ziskos_elf.rs
@@ -1,6 +1,9 @@
 use crate::spec::{Cc, CodeModel, LinkerFlavor, Lld, PanicStrategy};
 use crate::spec::{RelocModel, SanitizerSet, Target, TargetOptions};
 
+// The PSP has custom linker requirements.
+const LINKER_SCRIPT: &str = include_str!("./riscv64ima_polygon_ziskos_elf_linker_script.ld");
+
 pub fn target() -> Target {
     Target {
         data_layout: "e-m:e-p:64:64-i64:64-i128:128-n32:64-S128".into(),
@@ -28,6 +31,7 @@ pub fn target() -> Target {
             emit_debug_gdb_scripts: false,
             eh_frame_header: false,
             supported_sanitizers: SanitizerSet::KERNELADDRESS,
+            link_script: Some(LINKER_SCRIPT.into()),
             ..Default::default()
         },
     }
