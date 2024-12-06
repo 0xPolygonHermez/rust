be69e66d03c75804b5975422a737b2ff27d5d47b
agnusmor <agnusmor@gmail.com>
2024-12-06 13:55:36 +0100

    Change update-rust.sh: Remove ask user about push master branch after rebase it

--- scripts/update-rust.sh
+++ scripts/update-rust.sh
@@ -26,10 +26,10 @@ else
     echo -e "\e[1;32mRemote '${REMOTE_NAME}' already exists.\e[0m"
 fi
 
-# Fetch branches from the remote
-echo -e "\e[1;32mFetching 'master' branch from remote '${REMOTE_NAME}'\e[0m"
+# Fetch remote
+echo -e "\e[1;32mFetching remote '${REMOTE_NAME}'\e[0m"
 exec_git \
-    "git fetch ${REMOTE_NAME} master --tags" \
+    "git fetch ${REMOTE_NAME}" \
     "Failed to fetch remote '${REMOTE_NAME}' branches"
 
 # Ensure we are on the master branch
@@ -37,46 +37,38 @@ exec_git \
     "git checkout master" \
     "Failed to checkout 'master' branch"
 
-# Verify current branch
+# Verify current branch is master
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 if [[ "$current_branch" != "master" ]]; then
     echo -e "\e[1;31mCurrent branch is not 'master'.\e[0m"
     exit 1
 fi
 
-# Rebase master branch with upstream/master and capture its output
+# Update master branch rebasing it with upstream/master
 echo -e "\e[1;32mUpdating 'master' branch with 'upstream/master'\e[0m"
 exec_git \
     "git rebase upstream/master" \
     "Failed to rebase 'master' branch with 'upstream/master'"
-
-# Check if the remote upstream/master is ahead of the local master and ask if you want to push the changes
-commits_behind=$(git rev-list --count origin/master..master)
-if [[ "$commits_behind" -gt 0 ]]; then
-    read -p "$(echo -e '\e[1;33mDo you want to push the updated 'master' branch? [Y/n]: \e[0m')" response
-    response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
-
-    if [[ "$response" != "n" && "$response" != "no" ]]; then
-        echo -e "\e[1;32mPushing 'master' branch and tags to origin\e[0m"
-        exec_git \
-            "git push origin master --force" \
-            "Failed to push 'master' branch to origin"
-        exec_git \
-            "git push origin --tags" \
-            "Failed to push tags to origin"
-    fi
-fi
-
-# Fetch zisk-rust-${FROM_VERSION} branch
+echo -e "\e[1;32mPushing 'master' branch and tags to origin\e[0m"
 exec_git \
-    "git fetch origin zisk-rust-${FROM_VERSION}:zisk-rust-${FROM_VERSION}" \
-    "Failed to fetch 'zisk-rust-${FROM_VERSION}' branch"
+    "git push origin master --force" \
+    "Failed to push 'master' branch to origin"
+exec_git \
+    "git push origin --tags" \
+    "Failed to push tags to origin"
+
+exit 0
 
 # Create a new branch zisk-rust-${TO_VERSION} from ${TO_VERSION} branch
 echo -e "\e[1;32mCreating new branch 'zisk-rust-${TO_VERSION}' from ${TO_VERSION}\e[0m"
 exec_git \
     "git checkout -b zisk-rust-${TO_VERSION} ${TO_VERSION}" \
     "Failed to create new branch 'zisk-rust-${TO_VERSION}'"
+
+# Fetch zisk-rust-${FROM_VERSION} branch
+exec_git \
+    "git fetch origin zisk-rust-${FROM_VERSION}:zisk-rust-${FROM_VERSION}" \
+    "Failed to fetch 'zisk-rust-${FROM_VERSION}' branch"
 
 # List cherry picks
 echo -e "\e[1;32mList of cherry picks to apply:\e[0m"
@@ -93,6 +85,7 @@ echo "$commits"
 echo -e "\e[1;32mPress a key to continue...\e[0m"
 read -n1 -s
 
+# Apply cherry picks to branch zisk-rust-${TO_VERSION}
 for line in "${commits_array[@]}"; do
     # Check line starts with "+" (only apply added commits)
     if [[ "$line" =~ ^\+ ]]; then
